3e3428026bb9d516b201b38964f33c1ccdc86c51
Monokai <wimer@monokai.nl>
2014-09-22 14:52:49 +0200

    Added .js file

--- README.md
+++ README.md
@@ -2,3 +2,42 @@ ActionStack.js
 ==============
 
 Adds actions to a stack, so that actions that have already been queued can be triggered at a later time
+
+## Usage
+
+Queue a function, or immediately execute it if the stack is empty
+
+	ActionStack.add(function, [argument1, argument2, …]);
+
+Check for the next queued function and execute it
+
+	ActionStack.next();
+
+Check if a function and its arguments is the same as the previously executed function, returns `true` or `false`
+
+	ActionStack.isPrevious(function, [argument1, argument2, …]);
+
+## Example
+
+	function showInfo(id) {
+		if (ActionStack.isPrevious(showInfo, [id])) {
+			ActionStack.next();
+			return;
+		}
+
+		$('#info1, #info2').fadeOut(250, function() {
+			$(id).fadeIn(250, function() {
+				ActionStack.next();
+			});
+		})
+	}
+
+	$('#button1').on('click', function() {
+		ActionStack.add(showInfo, ['#info1']);
+	})
+
+	$('#button2').on('click', function() {
+		ActionStack.add(showInfo, ['#info2']);
+	})
+
+http://www.monokai.nl
---
+++ src/ActionStack.js
@@ -0,0 +1,112 @@
+/*!
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Monokai
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+modules.utils.ActionStack = (function() {
+
+	// this determines how many actions can be queued
+	var STACK_SIZE = 3;
+
+	var _stack;
+	var _previousID;
+
+	function init() {
+		_stack = [];
+	}
+
+	function hash(funk, args) {
+		var string = funk.toString() + args.join('-');
+
+		// hash the string to take less memory if the function is long (based on Java's hashCode)
+		var id = 0;
+
+		if (string.length === 0) {
+			return id;
+		}
+
+		for (var i = 0; i < string.length; i++) {
+			var c = string.charCodeAt(i);
+			id = ((id << 5) - id) + c;
+			id = id & id; // convert to 32 bit int
+		}
+
+		// returns 32 bit integer
+		return id;
+	}
+
+	function execute(o) {
+		// execute the action and store the function ID after that
+		o.funk.apply(o, o.args);
+		_previousID = o.id;
+	}
+
+	function add(funk, args) {
+		if (_stack.length > STACK_SIZE) {
+			return;
+		}
+
+		var o = {
+			id		: hash(funk, args),
+			funk	: funk,
+			args	: args
+		};
+
+		if (_stack.length) {
+			// add action to the stack if it's different from the last action on the stack
+			if (_stack[_stack.length - 1].id != o.id) {
+				_stack.push(o);
+			}
+		} else {
+			// execute the action if it's the first on the stack
+			_stack.push(o);
+			execute(o);
+		}
+	}
+
+	function next() {
+		if (_stack.length) {
+			// remove the first action on the stack and execute the next action
+			_stack.shift();
+			if (_stack.length) {
+				execute(_stack[0]);
+			}
+		}
+	}
+
+	function isPrevious(funk, args) {
+		// checks if the previous action is the same
+		var id = hash(funk, args);
+		return _previousID == id;
+	}
+
+	init();
+
+	return {
+		add			: add,
+		next		: next,
+		isPrevious	: isPrevious
+	};
+
+})();
